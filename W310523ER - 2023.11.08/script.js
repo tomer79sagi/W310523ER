9084e2155c26409d9e40d44822058a9e5b2dbf26
Daniel Zadok <zadok.daniel@gmail.com>
2023-11-08 21:37:54 +0200

    conversions

---
+++ W310523ER - 2023.11.08/script.js
@@ -0,0 +1,58 @@
+/*** TYPE CONVERSIONS */
+
+// FALSY VALUES
+console.log("", Boolean(""));
+
+console.log(0, Boolean(0));
+console.log(-0, Boolean(-0));
+console.log(NaN, Boolean(NaN));
+
+console.log(false, Boolean(false));
+
+console.log(null, Boolean(null));
+
+console.log(undefined, Boolean(undefined));
+
+// TRUTHY VALUES
+console.log("       ", Boolean("       "));
+console.log(" ", Boolean(" "));
+console.log("ads4", Boolean("ads4"));
+console.log(-55, Boolean(-55));
+console.log(5, Boolean(5));
+console.log(0.00000000001, Boolean(0.00000000001));
+console.log(true, Boolean(true));
+console.log({}, Boolean({}));
+
+/** Convert to String */
+
+console.log("to string");
+
+console.log(false, String(false));
+console.log(true, String(true));
+console.log(4343, String(4343));
+console.log(4343.444, String(4343.444));
+console.log(null, String(null));
+console.log(undefined, String(undefined));
+console.log({}, String({}));
+
+console.log(new Date(), String(new Date()));
+console.log(new Date(), "with .toString()", new Date().toString());
+console.log(new Date(), "with .toString()", {}.toString());
+
+// Number
+console.log(Number(true));
+
+console.log(Number(false));
+console.log(Number(null));
+console.log(Number(""));
+console.log(Number(" "));
+
+console.log(Number(undefined));
+console.log(Number("dskfjasd"));
+console.log(Number("4343adskfjasd"));
+console.log(Number("      233a2.3232"));
+console.log(Number("      23 32.3232       "));
+
+console.log(Number("2332.3232"));
+console.log(Number("      2332.3232"));
+console.log(Number("      2332.3232       "));
---
+++ W310523ER - 2023.11.08/script1.js
@@ -0,0 +1,50 @@
+// HW example
+
+console.log({
+  "hi how are you?": {
+    name: "daniel", 
+    email: "z@gmail.com", 
+    date: new Date()
+  },
+});
+
+
+if (5 === 5) {
+  console.log(1);
+  console.log(2);
+}
+
+
+if(5==5) {
+  let a = {}
+
+  a: 5, 
+  b: 9
+}
+
+
+
+let a = {
+  key: 5,
+
+
+  if() {}
+  for(){}
+}
+
+
+let a1 = "434"
+let a2  = 453
+let a3 = true
+let a4 = null
+let a5 = undefined
+
+let a6 = {
+  name: "daniel", 
+  age: 29,
+  address: {
+    city: "holon", 
+    number: 10
+  } 
+
+}
