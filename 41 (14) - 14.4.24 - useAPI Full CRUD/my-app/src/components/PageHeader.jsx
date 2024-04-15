732ab28799898cc59c4f47bd0d3c52e387f3fe9f
Tomer Sagi <me@tomersagi.com>
2024-04-15 17:04:21 +0300

    Lesson 41 (14) commit. Implementation of GET_ALL, GET_ONE and partial CREATE

---
+++ 41 (14) - 14.4.24 - useAPI Full CRUD/my-app/src/components/PageHeader.jsx
@@ -0,0 +1,9 @@
+const PageHeader = (props) => {
+    return ( 
+        <div className="center">
+            <h1>{ props.title }</h1>
+        </div>
+     );
+}
+ 
+export default PageHeader;
---
+++ 41 (14) - 14.4.24 - useAPI Full CRUD/my-app/src/components/product/ProductListNoHook.jsx
@@ -0,0 +1,148 @@
+import React, { useState, useEffect, useReducer } from 'react'
+import { PRODUCTS } from '../../models/Product';
+import ProductNew from './ProductNew';
+import ProductEdit from './ProductEdit';
+import './ProductList.css';
+import axios from 'axios';
+
+const ProductList = () => {
+
+    const UI_STATE = {
+        NONE: 'NONE',
+        CREATE: 'CREATE',
+        EDIT: 'EDIT'
+    }
+
+    // ** useReducer hook stuff **
+    // 1. Define the ACTION TYPES
+    const ACTION_TYPES = {
+        PRODUCTS_RECEIVED: 'PRODUCTS_RECEIVED',
+        PRODUCT_CREATE: 'PRODUCT_CREATE',
+        PRODUCT_CREATED: 'PRODUCT_CREATED',
+        PRODUCT_EDIT: 'PRODUCT_EDIT',
+        PRODUCT_UPDATED: 'PRODUCT_UPDATED'
+    }
+
+    // 2. Define the REDUCER function
+    // INPUT: 'state' and 'action' objects
+    // OUTPUT (return): new 'state'
+    const reducer = (state, action) => {
+        switch (action.type) {
+
+            case ACTION_TYPES.PRODUCTS_RECEIVED:
+                return { ...state, products: action.payload };
+
+            case ACTION_TYPES.PRODUCT_CREATE:
+                return { ...state, uiState: UI_STATE.CREATE };
+
+            case ACTION_TYPES.PRODUCT_CREATED:
+                return { ...state, products: [...state.products, action.payload], uiState: UI_STATE.NONE };
+
+            case ACTION_TYPES.PRODUCT_EDIT:
+                return { ...state, selectedProduct: action.payload, uiState: UI_STATE.EDIT };
+
+            case ACTION_TYPES.PRODUCT_UPDATED:
+                const productsNew = state.products.map(eProduct => eProduct.id === action.payload.id ? action.payload : eProduct);
+                return { ...state, selectedProduct: null, products: productsNew, uiState: UI_STATE.NONE };
+        }
+    }
+
+    // 3. Define the initial state of the reducer
+    const initialState = {
+        products: PRODUCTS,
+        selectedProduct: null,
+        uiState: UI_STATE.NONE
+    }
+
+    // 4. Initialize the 'useReducer' hook
+    const [state, dispatch] = useReducer(reducer, initialState);  
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    
+    useEffect(() => {
+       // 1. Define async API function
+       const fetchProducts = async () => {
+            try {
+                setIsLoading(true);
+                const response = await axios.get('https://fakestoreapi.com/products');
+                const appProducts = response.data.map(apiProduct => {
+                    apiProduct.name = apiProduct.title;
+                    delete apiProduct.title;
+                    return apiProduct;
+                });
+
+                dispatch({type: ACTION_TYPES.PRODUCTS_RECEIVED, payload: appProducts});
+            } catch(err) {
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        // 2. Call the async function
+        fetchProducts();        
+    }, []);
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error: { error }</div>
+    if (!state.products) return '<div>No results found</div>';
+
+    return (
+        <div className="mainScreen">
+
+            <div>
+                <h2 className='center'>Product List</h2>
+                <button onClick={() => dispatch({type: ACTION_TYPES.PRODUCT_CREATE, payload: null})}>Add New Product</button>
+            </div>
+
+            <div className='mainContent'>
+
+                <div>
+
+                    <table>
+
+                        <tbody>
+
+                            { state.products.map(product => (
+                                <tr key={product.id}>
+                                    <td>{ product.id }</td>
+                                    <td>{ product.name }</td>
+                                    <td>{ product.price }</td>
+                                    <td>
+                                        {/* <button onClick={() => alert(`${product.id} - ${product.name}`)}>Show Details</button> */}
+                                        <button onClick={() => alert(`${product.id} - ${product.name}`)}>Show</button>
+                                        <button onClick={() => dispatch({type: ACTION_TYPES.PRODUCT_EDIT, payload: product }) }>Edit</button>
+                                    </td>
+                                </tr>
+                            ))}
+
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+                <div className="detailsPanel">
+
+                    <div>
+
+                        {/* { uiState === UI_STATE.CREATE && <ProductNew callbackSuccess={productCreated}/> }    */}
+
+                        { state.uiState === UI_STATE.CREATE && <ProductNew
+                            callbackSuccess={(product) => dispatch({ type: ACTION_TYPES.PRODUCT_CREATED, payload: product }) }/> }  
+
+                        { state.uiState === UI_STATE.EDIT && <ProductEdit 
+                            selectedProduct={state.selectedProduct}
+                            callbackSuccess={(product) => dispatch({ type: ACTION_TYPES.PRODUCT_UPDATED, payload: product })}/> }   
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductList;
